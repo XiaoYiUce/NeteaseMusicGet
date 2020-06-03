fbf90c202aa0bd0d907bb709e24ee62251e7b47f
XiaoYiUce <tyeasyuce@gmail.com>
2020-06-03 23:05:28 +0800

    加入彩色标识 修正了一些小问题

--- NeteaseMusicGet/Program.cs
+++ NeteaseMusicGet/Program.cs
@@ -1,13 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Web;
 using System.Runtime.InteropServices;
-using System.IO;
-using Core;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System.Linq;
+using System.Web;
 
 namespace NeteaseMusicGet
 {
@@ -23,33 +22,48 @@ namespace NeteaseMusicGet
 
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" _ _        _                      __ __           _       ___         _   ");
+            Console.WriteLine("| \\ | ___ _| |_ ___  ___  ___ ___ |  \\  \\ _ _  ___<_> ___ /  _>  ___ _| |_ ");
+            Console.WriteLine("|   |/ ._> | | / ._><_> |<_-</ ._>|     || | |<_-<| |/ | '| <_/\\/ ._> | |  ");
+            Console.WriteLine("|_\\_|\\___. |_| \\___.<___|/__/\\___.|_|_|_|`___|/__/|_|\\_|_.`____/\\___. |_| ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("[Info]正在初始化");
             //判断平台
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) == true)
             {
                 symbol = "\\";
+                Console.WriteLine("[Info]当前平台为Windows");
             }
             else //如果不为Windows平台，统一使用Unix及类Unix路径
             {
                 symbol = "/";
+                Console.WriteLine("[Info]当前平台为Linux/OSX/类Unix");
             }
 
-            if (File.Exists(Environment.CurrentDirectory+symbol+"config.json")==true) //判断config.json是否存在
+            Console.WriteLine("[Info]正在载入配置");
+
+            if (File.Exists(Environment.CurrentDirectory + symbol + "config.json") == true) //判断config.json是否存在
             {
                 StreamReader file = File.OpenText(Environment.CurrentDirectory + symbol + "config.json");
                 JsonTextReader reader = new JsonTextReader(file);
                 JObject jsonObject = (JObject)JToken.ReadFrom(reader);
                 Cookie = (string)jsonObject["Cookie"];
                 Nickname = (string)jsonObject["Name"];
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("[Info]欢迎回来," + Nickname + "!");
             }
             else //如果不为真，则执行登陆操作
             {
-                login(symbol);             
+                Console.WriteLine("[Info]没有找到配置文件，执行初始化");
+                login(symbol);
                 /*HttpResponseMessage responseinfo = Common.httpClient.GetAsync(httpurl).Result; //访问url并取回数据*/
                 /*var cookie = responseinfo.Headers.GetValues("Set-Cookie");*/
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("您好," + Nickname + "!");
             }
-
-            Console.WriteLine("您好," + Nickname + "!");
-            Core.Core.MusicSearch(symbol,Cookie);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Core.Core.MusicSearch(symbol, Cookie);
         }
 
         /// <summary>
@@ -66,34 +80,35 @@ namespace NeteaseMusicGet
         /// </summary>
         static void login(string symbol)
         {
-            Console.WriteLine("请选择登陆方式(1为邮箱，2为手机)");
+            Console.WriteLine("[Info]请选择登陆方式(1为邮箱，2为手机)");
             String loginmethod = Console.ReadLine();
             int method = Int32.Parse(loginmethod);
             if (method == 1) //如果所选模式为1
             {
-                Console.WriteLine("请输入您的邮箱账号");
+                Console.WriteLine("[Info]请输入您的邮箱账号");
                 username = Console.ReadLine();
-                Console.WriteLine("请输入您的密码");
+                Console.WriteLine("[Info]请输入您的密码");
                 password = HttpUtility.HtmlEncode(Console.ReadLine());
                 url = "http://server2.odtm.tech:3000/login?email=" + username + "&password=" + password;
             }
             else if (method == 2)//如果所选模式为2
             {
-                Console.WriteLine("请输入您的中国大陆手机号");
+                Console.WriteLine("[Info]请输入您的中国大陆手机号");
                 String phonenumber = Console.ReadLine();
-                Console.WriteLine("请输入您的密码");
+                Console.WriteLine("[Info]请输入您的密码");
                 String password = HttpUtility.HtmlEncode(Console.ReadLine());
                 url = "http://server2.odtm.tech:3000/login/cellphone?phone=" + phonenumber + "&password=" + password;
             }
             else
             {
-                Console.WriteLine("您输入的数值无效，请输入正确数值!");
-                Console.ReadKey();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("[Err]您输入的数值无效，请输入正确数值!");
+                Console.ForegroundColor = ConsoleColor.Green;
                 login(symbol);
             }
 
-
-            var httpUri=new Uri(url);
+            Console.WriteLine("[Info]正在登陆");
+            var httpUri = new Uri(url);
             var httphandler = new HttpClientHandler();
             httphandler.UseCookies = true;
             httphandler.CookieContainer = new CookieContainer();
@@ -116,9 +131,17 @@ namespace NeteaseMusicGet
                 File.WriteAllText(FileSaveDir, JsonConvert.SerializeObject(info)); //序列化并保存JSON
                 Nickname = info.Name;
             }
+            else if(result == "")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("[Err]网络错误!程序即将退出");
+                return;
+            }
             else
             {
-                Console.WriteLine("账号或密码错误!请重新输入");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("[Err]账号或密码错误!请重新输入");
+                Console.ForegroundColor = ConsoleColor.Green;
                 login(symbol);
             }
         }
