2789182f5588eff5f6944e32384a98c6059f8922
XiaoYiUce <tyeasyuce@gmail.com>
2020-06-03 16:06:53 +0800

    整理文件

--- NeteaseMusicGet/Core.cs
+++ NeteaseMusicGet/Core.cs
@@ -108,7 +108,7 @@ namespace Core
                         }
                         else
                         {
-                            MusicInfo = MusicInfo + Singer2["name"] + "/";
+                            MusicInfo = MusicInfo + Singer2["name"] + "";
                         }
                     }
                     MusicInfo = MusicInfo + "\r\n";
