6e6db3d8de18b32b082380c626ac1f22b071cced
XiaoYiUce <xiaoyi@Onedreamteam.site>
2020-06-03 14:22:54 +0800

    首个版本

---
+++ NeteaseMusicGet/Core.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Core
+{
+
+    public class Core
+    {
+        /// <summary>
+        /// 检测输入字符是否为纯整数
+        /// </summary>
+        /// <param name="sNum"></param>
+        /// <returns>返回值为true或者false</returns>
+        static bool IsNumber(string str)
+        {
+            System.Text.RegularExpressions.Regex reg1 = new System.Text.RegularExpressions.Regex(@"^[0-29]\d*$");
+            return reg1.IsMatch(str);
+        }
+
+        public static string FileName;
+        public static string SingerName;
+        public static string MusicName;
+        public static string MusicDownloadLink;
+        public static string MusicInfo;
+        public static int MusicNumber;
+
+        public static void MusicDownload(string url, string MusicName, string SingerName,string symbol) //MusicDownload子程序
+        {
+            ///<summary>
+            ///此子程序用于下载音乐，第一个值为音乐文件的URL，第二个参数为音乐名称，第三个参数为歌手名称，第四个名称为标识符
+            ///</summary>
+
+            WebClient client = new WebClient();
+            string ProgramRunDirectory = Environment.CurrentDirectory;
+            string patternflac = @"(.*)(\.flac)$";
+            string patternncm = @"(.*)(\.ncm)$";
+            string patternmp3 = @"(.*)(\.mp3)$";
+            if (Regex.IsMatch(url, patternflac))
+            {
+                FileName = MusicName + "-" + SingerName + ".flac";
+            }
+            else if (Regex.IsMatch(url, patternncm))
+            {
+                FileName = MusicName + "-" + SingerName + ".ncm";
+            }
+            else if (Regex.IsMatch(url, patternmp3))
+            {
+                FileName = MusicName + "-" + SingerName + ".mp3";
+            }
+
+            string SaveDirectory = ProgramRunDirectory + symbol + FileName;
+            client.DownloadFile(url, SaveDirectory);
+            Console.WriteLine("文件已保存至:" + SaveDirectory);
+        }
+
+        ///<summary>
+        ///此方法用于检索音乐并执行下载，需填写symbol（平台路径符号）
+        ///</summary>
+        public static void MusicSearch(string symbol,string Cookie)
+        {
+            Console.WriteLine("请输入您要搜索的歌曲名称");
+            String SongName = Console.ReadLine();
+            if (SongName == "") 
+            {
+                Console.WriteLine("请填入正确的关键词!");
+                MusicSearch(symbol, Cookie);
+            }
+
+            if (SongName.Trim() == string.Empty)
+            {
+                Console.WriteLine("请填入正确的关键词!");
+                MusicSearch(symbol, Cookie);
+            }
+
+            string SearchSongurl = "http://server2.odtm.tech:3000/search?keywords=" + SongName;
+            var httpClient = new HttpClient();
+            httpClient.Timeout = TimeSpan.FromSeconds(20); //设定超时时间为20ms
+            string UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0"; //定义UA
+            httpClient.DefaultRequestHeaders.Add("user-agent", UA); //将UA添加至请求头中
+            httpClient.DefaultRequestHeaders.Add("Cookie", Cookie);//添加Cookie
+            HttpResponseMessage response = httpClient.GetAsync(new Uri(SearchSongurl)).Result; //使用GET方法获取JSON
+            String result = response.Content.ReadAsStringAsync().Result; //定义result变量
+            JObject JsonReader = JObject.Parse(result);
+            JArray List = JArray.Parse(JsonReader["result"]["songs"].ToString());
+            for (int i = 0; List.Count > i ; ++i)
+            {
+                JObject json = JObject.Parse(List[i].ToString());
+                MusicInfo = MusicInfo + "[" + i + "]"+"曲名:"+json["name"]+" 歌手:"+json["artists"][0]["name"]+"\r\n";
+            }
+            Console.WriteLine(MusicInfo);
+            MusicInfo = ""; //清空变量
+            Console.WriteLine("请输入您需要下载的歌曲编号");
+            string MusicNumberTemp = Console.ReadLine();
+            if(IsNumber(MusicNumberTemp) == true)
+            {
+                MusicNumber = Int32.Parse(MusicNumberTemp);
+                JObject json2 = JObject.Parse(List[MusicNumber].ToString());
+                String MusicID = (string)json2["id"];
+                MusicName = (string)json2["name"];
+                SingerName = (string)json2["artists"][0]["name"];
+                Console.WriteLine("歌曲名称:" + MusicName + " 歌曲ID:" + MusicID + " 歌手:" + SingerName);
+                Console.WriteLine("准备开始下载");
+                HttpResponseMessage response3 = httpClient.GetAsync(new Uri("http://server2.odtm.tech:3000/song/url?id=" + MusicID)).Result; //获取歌曲信息JSON
+                JObject JsonReader2 = JObject.Parse(response3.Content.ReadAsStringAsync().Result);
+                MusicDownloadLink = (string)JsonReader2["data"][0]["url"];
+                if (MusicDownloadLink == null)
+                {
+                    Console.WriteLine("无法获取下载地址，请检查您是否有网易云VIP权限或歌曲对应专辑");
+                    Console.WriteLine("请按任意键继续...");
+                    Console.ReadKey();
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("歌曲下载地址为:" + MusicDownloadLink);
+                    MusicDownload(MusicDownloadLink, MusicName, SingerName, symbol);
+                }
+            }
+            else
+            {
+                Console.WriteLine("您键入的数值有误!请重新搜索");
+                MusicSearch(symbol,Cookie);
+            }
+
+        }
+    }
+   
+}
---
+++ NeteaseMusicGet/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Runtime.InteropServices;
+using System.IO;
+using Core;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace NeteaseMusicGet
+{
+
+    class Program
+    {
+        public static string username;
+        public static string password;
+        public static string url;
+        public static string symbol;
+        public static string Cookie;
+        public static string Nickname;
+
+        static void Main(string[] args)
+        {
+            //判断平台
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) == true)
+            {
+                symbol = "\\";
+            }
+            else //如果不为Windows平台，统一使用Unix及类Unix路径
+            {
+                symbol = "/";
+            }
+
+            if (File.Exists(Environment.CurrentDirectory+symbol+"config.json")==true) //判断config.json是否存在
+            {
+                StreamReader file = File.OpenText(Environment.CurrentDirectory + symbol + "config.json");
+                JsonTextReader reader = new JsonTextReader(file);
+                JObject jsonObject = (JObject)JToken.ReadFrom(reader);
+                Cookie = (string)jsonObject["Cookie"];
+                Nickname = (string)jsonObject["Name"];
+            }
+            else //如果不为真，则执行登陆操作
+            {
+                login(symbol);             
+                /*HttpResponseMessage responseinfo = Common.httpClient.GetAsync(httpurl).Result; //访问url并取回数据*/
+                /*var cookie = responseinfo.Headers.GetValues("Set-Cookie");*/
+            }
+
+            Console.WriteLine("您好," + Nickname + "!");
+            Core.Core.MusicSearch(symbol,Cookie);
+
+            /*String MusicLink = */
+            Console.WriteLine("如还需下载歌曲，请输入1，否则按任意键退出");
+            String method = Console.ReadLine();
+
+            if (method == "1")
+            {
+                Core.Core.MusicSearch(symbol,Cookie);
+            }
+            else
+            {
+                return;
+            }
+
+
+        }
+
+        /// <summary>
+        /// 用户信息实体
+        /// </summary>
+        public class UserInfo
+        {
+            public string Name { get; set; }
+            public string Cookie { get; set; }
+        }
+
+        /// <summary>
+        /// 网易云登陆模块，用于用户登陆并保存Cookie
+        /// </summary>
+        static void login(string symbol)
+        {
+            Console.WriteLine("请选择登陆方式(1为邮箱，2为手机)");
+            String loginmethod = Console.ReadLine();
+            int method = Int32.Parse(loginmethod);
+            if (method == 1) //如果所选模式为1
+            {
+                Console.WriteLine("请输入您的邮箱账号");
+                username = Console.ReadLine();
+                Console.WriteLine("请输入您的密码");
+                password = HttpUtility.HtmlEncode(Console.ReadLine());
+                url = "http://server2.odtm.tech:3000/login?email=" + username + "&password=" + password;
+            }
+            else if (method == 2)//如果所选模式为2
+            {
+                Console.WriteLine("请输入您的中国大陆手机号");
+                String phonenumber = Console.ReadLine();
+                Console.WriteLine("请输入您的密码");
+                String password = HttpUtility.HtmlEncode(Console.ReadLine());
+                url = "http://server2.odtm.tech:3000/login/cellphone?phone=" + phonenumber + "&password=" + password;
+            }
+            else
+            {
+                Console.WriteLine("您输入的数值无效，请输入正确数值!");
+                Console.ReadKey();
+                login(symbol);
+            }
+
+
+            var httpUri=new Uri(url);
+            var httphandler = new HttpClientHandler();
+            httphandler.UseCookies = true;
+            httphandler.CookieContainer = new CookieContainer();
+
+            var responseContext = new HttpClient(httphandler).GetAsync(httpUri).Result;
+            String result = responseContext.Content.ReadAsStringAsync().Result; //定义result变量
+            JObject JsonReader = JObject.Parse(result); //反序列化对象
+            int code = (int)JsonReader["code"];
+            if (code == 200) //如果返回成功状态码
+            {
+                foreach (var cookieContext in httphandler.CookieContainer.GetCookies(httpUri).Cast<Cookie>().ToList())
+                {
+                    Cookie = Cookie + cookieContext.Name + "=" + cookieContext.Value + ";";  //定义Cookie
+                }
+
+                UserInfo info = new UserInfo();
+                info.Name = (string)JsonReader["profile"]["nickname"];
+                info.Cookie = Cookie;
+                string FileSaveDir = Environment.CurrentDirectory + symbol + "config.json";
+                File.WriteAllText(FileSaveDir, JsonConvert.SerializeObject(info)); //序列化并保存JSON
+                Nickname = info.Name;
+            }
+            else
+            {
+                Console.WriteLine("账号或密码错误!请重新输入");
+                login(symbol);
+            }
+        }
+
+
+    }
+}
